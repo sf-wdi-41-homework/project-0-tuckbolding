e61d92b4cdebcd45752bb664ad26343317e889b5
tuckbolding <tuckbolding@gmail.com>
2017-10-08 16:36:20 -0700

    added keypress event listener and made images move once when clicked

--- JS/app.js
+++ JS/app.js
@@ -1,37 +1,38 @@
 //Create Gameboard
   //must have startline, Grid to move cars up, and finishline to determine winner
-var Gameboard;
 
 var player1Grid =[];
 var player2Grid =[];
-//Create Cars
-  // 2 players have separate cars assigned to different buttons
-var player1Img = "assets/red-racecar.png";
-var player2Img = "assets/pink-racecar.png";
 
+//Establish doc.ready function to listen for keypresses
+$(document).ready(function(){
 
-    function createBoard(){
-
+  // listen for keypress event on the document
+  $(document).keypress(function(event){
+    if(event.which === 102){
+      player1Move();
+      console.log("pressed: " + event.which);
     }
+  })
 
-    //Start Game by putting cars on start line
-    function startGame(){
-      $('.row 1')
-    }
+
+});
+
+
     //Make the Cars move
       //cars move a grid space on button click (using jquery)
     function player1Move(){
           //Player1 move
-            $('.row').keypress(){
-              console.log("player 1 moved!");
-          }
+            $('.player1').css('left', function( left ) {
+              return left + 50;
+            });
     }
     //Player2 move
     function player2Move(){
           //Player2 move
-            if($('.row').keypress(){
-              console.log("player 2 moved!");
-          }
+          $('.player2').css('left', function( left ) {
+            return left + 50;
+          });
     }
     //Check for winner
       //after each button click, check to see if Car value === finishline value in Grid array
